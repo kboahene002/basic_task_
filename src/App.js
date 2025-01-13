be5f614f9b4099144b0f0469b2f888ae3681be17
Kelvin Boahene <kelvin@rxhealthinfosystems.com>
2025-01-13 11:26:33 +0000

    basic task project with react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Task from '../src/components/Task';
+// import logo from './logo.svg';
+import '../src/css/task.css';
+
+function App() {
+  return (
+
+    <>
+      <Task/>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Task.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+function Task() {
+    let [tasks, setTasks] = useState([
+        {title:"Complete todays assignment", id:1, completed: false},
+        {title:"Get groceries from the shop", id:2, completed: false},
+        {title:"Learn React js from scratch", id:3, completed: false},
+        {title:"lets Pray and Hope for success", id:4, completed: true}
+    ]);
+    function changeStatus(taskid){
+        // let new_task = tasks.filter( (task) => task.id !== taskid);
+        // setTasks(new_task);
+
+        let new_task = tasks.map((task) => {
+            if(task.id === taskid){
+                task.completed = !task.completed;
+            }
+            return task;
+        });
+        setTasks(new_task);
+    }
+    return (
+        <div>
+            <div className="main_div">
+            <h2 className="heading">Task List</h2>
+
+                <ul className="task_list">
+                    {tasks.map(task => (
+                        <li className={task.completed == false ? "flex-item active" : "flex-item inactive"} key={task.id}>
+                            <h4 className="task_title">{task.title}</h4>
+                            <button className={task.completed == false ? "complete" : "completed"} onClick={() => changeStatus(task.id)}>
+                                    {task.completed == false ? "Complete" : " Completed"}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
+export default Task;
